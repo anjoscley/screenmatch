07a417d091e99007ee6489f1562334b12045b6f8
Dariclei Anjos <anjoscley@gmail.com>
2024-07-06 18:34:38 -0300

    Feat: Implementado consumo da api

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
 
@@ -15,6 +19,15 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("Primeiro projeto spring sem web!");
 		
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=b5f6025a");
+		//System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		
+		System.out.println(dados);
 	}
 
 }
